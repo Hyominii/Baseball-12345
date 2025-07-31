651ca2cbf72b8031e760337d4cbde445feaa735d
hyomin <dksgyals1@gmail.com>
2025-07-31 14:08:47 +0900

    [refactoring] p.46

--- game.py
+++ game.py
@@ -6,14 +6,17 @@ class Game:
 
     def _assert_illegal_value(self, guess_num):
         if guess_num is None:
-            raise TypeError()
+            raise TypeError("입력이 None 입니다.")
+
         if len(guess_num) != 3:
-            raise TypeError()
+            raise TypeError("입력은 3 자리 문자열이어야 합니다.")
+
         for number in guess_num:
             if not ord('0') <= ord(number) <= ord('9'):
-                raise TypeError()
+                raise TypeError("모든 문자는 숫자로 구성되어야 합니다.")
+
         if self._is_duplicated_number(guess_num):
-            raise TypeError()
+            raise TypeError("중복된 숫자가 존재합니다.")
 
     def _is_duplicated_number(self, guess_num):
         return guess_num[0] == guess_num[1] or \
