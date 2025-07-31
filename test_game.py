a56cf4e4834d1692dee02068b90becaa95348111
hyomin <dksgyals1@gmail.com>
2025-07-31 14:34:33 +0900

    [refactoring] p.64

--- test_game.py
+++ test_game.py
@@ -19,7 +19,7 @@ def test_exception_when_invalid_input(game, invalid_input):
     assert_illegal_argument(game, invalid_input)
 
 
-def assert_matched_number(balls, result, solved, strikes):
+def assert_matched_number(result, solved, strikes, balls):
     assert result is not None
     assert result.solved == solved
     assert result.strikes == strikes
@@ -44,4 +44,4 @@ def test_return_solved_result_if_unmatched_number(game):
     strikes = 0
     balls = 0
 
-    assert_matched_number(balls, result, solved, strikes)
+    assert_matched_number(result, solved, strikes, balls)
