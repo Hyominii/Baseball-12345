f01e2f4e881acbf6ba619d6dbd94725109c1057f
hyomin <dksgyals1@gmail.com>
2025-07-31 14:11:45 +0900

    [refactoring] p.47

--- game.py
+++ game.py
@@ -8,17 +8,14 @@ class Game:
         if guess_num is None:
             raise TypeError("입력이 None 입니다.")
 
-        if len(guess_num) != 3:
+        if not isinstance(guess_num, str) or len(guess_num) != 3:
             raise TypeError("입력은 3 자리 문자열이어야 합니다.")
 
-        for number in guess_num:
-            if not ord('0') <= ord(number) <= ord('9'):
-                raise TypeError("모든 문자는 숫자로 구성되어야 합니다.")
+        if not guess_num.isdigit():
+            raise TypeError("모든 문자는 숫자로 구성되어야 합니다.")
 
         if self._is_duplicated_number(guess_num):
             raise TypeError("중복된 숫자가 존재합니다.")
 
     def _is_duplicated_number(self, guess_num):
-        return guess_num[0] == guess_num[1] or \
-            guess_num[0] == guess_num[2] or \
-            guess_num[1] == guess_num[2]
+        return len(set(guess_num)) != 3
