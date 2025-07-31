4a44820badea6e514f833734e7202a6801f23c5e
hyomin <dksgyals1@gmail.com>
2025-07-31 14:31:20 +0900

    [feature] p.60

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ class Game:
         self._assert_illegal_value(guess_num)
         if guess_num == self._question:
             return GameResult(True, 3, 0)
-        return None
+        return GameResult(False, 0, 0)
 
     def _assert_illegal_value(self, guess_num):
         if guess_num is None:
